78939800099d588f69e7b3fd6df889be33f3ec80
Arjun <103857050+AM10101010@users.noreply.github.com>
2026-05-21 15:28:06 +0200

    feat: add basic interactive console application

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Huvudmeny");
+Console.WriteLine("Skriv in siffror för att välja ett alternativ:")
+
+while (true)
+{
+    Console.WriteLine("1. Alternativ 1");
+    Console.WriteLine("2. Alternativ 2");
+    Console.WriteLine("3. Avsluta");
+
+    string input = Console.ReadLine();
+
+    if (input == "1")
+    {
+        Console.WriteLine("Du valde alternativ 1");
+    }
+    else if (input == "2")
+    {
+        Console.WriteLine("Du valde alternativ 2");
+    }
+    else if (input == "3")
+    {
+        Console.WriteLine("Avslutar programmet...");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Ogiltigt val, försök igen.");
+    }
+}
