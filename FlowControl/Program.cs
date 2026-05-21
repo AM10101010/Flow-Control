d4bc21493f3d3525d0076831b66bb020db8fc1a7
Arjun <103857050+AM10101010@users.noreply.github.com>
2026-05-21 16:02:48 +0200

    feat: add discounted pricing for youth and seniors

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -1,17 +1,37 @@
 ﻿Console.WriteLine("Huvudmeny");
-Console.WriteLine("Skriv in siffror för att välja ett alternativ:")
+Console.WriteLine("Skriv in siffror för att välja ett alternativ:");
 
 while (true)
 {
-    Console.WriteLine("1. Alternativ 1");
+    Console.WriteLine("1. Ungdom eller pensionär");
     Console.WriteLine("2. Alternativ 2");
     Console.WriteLine("3. Avsluta");
 
-    string input = Console.ReadLine();
+    string? input = Console.ReadLine();
 
     if (input == "1")
     {
-        Console.WriteLine("Du valde alternativ 1");
+        Console.WriteLine("Ange din ålder:");
+
+        // Allow null
+        string? ageInput = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(ageInput) && int.TryParse(ageInput, out int age))
+        {
+            if (age < 20)
+            {
+                Console.WriteLine("Ungdomspris: 80 kr");
+            }
+            else if (age >= 64)
+            {
+                Console.WriteLine("Pensionärspris: 90 kr");
+            }
+            else
+            {
+                Console.WriteLine("Standardpris: 120 kr");
+            }
+        }
+            
     }
     else if (input == "2")
     {
